e22ae5ed0bc50f5f06631d86503a44eed607af72
kirandash <thekirankumardash@gmail.com>
2020-03-24 15:51:42 +0800

    Django React Tourism - 5.1 Using timers with state to display data

--- django_react_tourism_ui/build/service-worker.js
+++ django_react_tourism_ui/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f4c9aa5d64a61bb27169560f36f65b2.js"
+  "/precache-manifest.9081a35b0ab886352e8f28d448080e1c.js"
 );
 
 self.addEventListener('message', (event) => {
